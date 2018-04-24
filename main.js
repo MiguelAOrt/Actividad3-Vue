f926ce1c0b993e7e84df4d33bbf0c60a6687431d
MiguelAOrt <36196115+MiguelAOrt@users.noreply.github.com>
2018-04-24 07:00:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const vm = new Vue({
+    el: 'main',
+    data:  {
+        lim_preguntas: 2,
+        preguntas: {
+        	pregunta: 'pregunta 1',
+        	descripcion: 'estas en una pregunta',
+        }
+    },
+});
